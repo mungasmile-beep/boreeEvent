8e33cc996daa64979df5dc81d916c662f39d140e
mungasmile-beep <mungasmile@gmail.com>
2026-07-04 15:02:10 +0200

    Ajout du  drinkChoice pour corriger erreur 500

--- demo/src/main/java/com/example/demo/model/Guest.java
+++ demo/src/main/java/com/example/demo/model/Guest.java
@@ -5,24 +5,31 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Entity
+@Table(name = "guest")
 @Data
 public class Guest {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "email")
     private String email;
 
     @ManyToOne
     @JoinColumn(name = "couple_id")
-    @JsonIgnore 
+    @JsonIgnore
     private Couple couple;
 
     @Column(name = "guest_table")
     private String table;
 
+    @Column(name = "status")
     private String status;
-    
-    // Le champ drinkChoice a été supprimé ici pour éviter l'erreur 500
+
+    @Column(name = "drink_choice")
+    private String drinkChoice;
 }
