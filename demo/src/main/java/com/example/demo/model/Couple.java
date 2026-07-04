65a93d1368c5c80ff435584afa62efb3f2245e82
mungasmile-beep <mungasmile@gmail.com>
2026-07-04 13:53:20 +0200

    Nettoyage BDD et mise à jour configuration

--- demo/src/main/java/com/example/demo/model/Couple.java
+++ demo/src/main/java/com/example/demo/model/Couple.java
@@ -2,6 +2,7 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 import lombok.Data;
 @Entity
+@Table(name = "couple") // Ajoutez cette ligne pour forcer le nom de la table
 @Data
 public class Couple {
     @Id
--- demo/src/main/resources/schema.sql
+++ demo/src/main/resources/schema.sql
@@ -1,17 +1,18 @@
 -- Création de la table couple
 CREATE TABLE IF NOT EXISTS couple (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     nom_partenaire1 VARCHAR(255),
     nom_partenaire2 VARCHAR(255),
     name VARCHAR(255),
     datemariage DATE,
-    photo LONGTEXT
+    photo TEXT
 );
--- Création de la table guest avec la structure nécessaire
+
+-- Création de la table guest
 CREATE TABLE IF NOT EXISTS guest (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     guest_table VARCHAR(255),
     confirmed BOOLEAN DEFAULT FALSE,
     couple_id BIGINT,
-    FOREIGN KEY (couple_id) REFERENCES couple(id)
+    CONSTRAINT fk_couple FOREIGN KEY (couple_id) REFERENCES couple(id)
 );
